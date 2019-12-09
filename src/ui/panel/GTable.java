db0e50b4333c03d1a75931e75f1ea7b9dd429d53
xiaoduan <drcxd@sina.com>
2019-12-08 19:32:22 -0500

    fix some bug occured when building header of table

--- src/ui/panel/GTable.java
+++ src/ui/panel/GTable.java
@@ -16,25 +16,26 @@ public class GTable extends JPanel{
     public void update(){
         setLayout(new GridBagLayout());
         Component root = Component.buildTestComponent();
-        buildHeader(root, 0, 0, root.getWidth()*5, root.getHeight());
+        buildHeader(root, 0, 0, root.getWidth(), root.getHeight());
     }
 
     private void buildHeader(Component root, int x, int y, int w, int max_h){
         if(!root.children.isEmpty()) {
-            GridBagConstraints gbc_new = new GridBagConstraints(x, y, w, 1, 1.0, 1.0,
+            GridBagConstraints gbc_new = new GridBagConstraints(x, y, w, 1, 0.0, 0.0,
                     GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0,0,0,0), 0, 0);
             JButton btn = new JButton(root.name);
             add(btn, gbc_new);
             int i = 0;
+            int _w = 0;
             for (Map.Entry<Integer, Component> entry: root.children.entrySet()){
-                int _w = w / root.children.entrySet().size();
-                int _x = x + i * _w;
+                int _x = x + _w;
                 int _y = y + 1;
-                buildHeader(root.children.get(entry.getKey()), _x, _y, _w, max_h);
-                i++;
+                buildHeader(entry.getValue(), _x, _y, entry.getValue().getWidth(), max_h);
+                _w += entry.getValue().getWidth();
+                ++i;
             }
         } else {
-            GridBagConstraints gbc_new = new GridBagConstraints(x, y, w, max_h - y, 1.0, 1.0,
+            GridBagConstraints gbc_new = new GridBagConstraints(x, y, w, max_h - y, 0.0, 0.0,
                     GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0,0,0,0), 0, 0);
             JButton btn = new JButton(root.name);
             add(btn, gbc_new);
