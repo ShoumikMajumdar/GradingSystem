3acd61a6beb29d81b173d310418e94e4682dc386
Fuqing Wwang <wangfuqing80@gmail.com>
2019-12-09 19:29:38 -0500

    graph header done

--- src/ui/panel/TablePanel.java
+++ src/ui/panel/TablePanel.java
@@ -10,8 +10,10 @@ public class TablePanel extends JPanel {
     }
 
     public void addTable(){
+        JPanel jp = new JPanel();
+        jp.add(new GTable());
         JScrollPane jsp = new JScrollPane(new GTable());
-        jsp.setHorizontalScrollBar(new JScrollBar(JScrollBar.HORIZONTAL));
+//        jsp.setHorizontalScrollBar(new JScrollBar(JScrollBar.HORIZONTAL));
 //        JViewport jv = new JViewport();
 //        jv.setSize(UIConsts.MAIN_WINDOW_WIDTH, UIConsts.MAIN_WINDOW_HEIGHT);
 //        jsp.setViewport(jv);
